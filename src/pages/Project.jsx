cca7cd4d6544993a156a78e457cd2cd2bee2bbb1
ryanpurnomo83 <ryanpurnomo83@gmail.com>
2025-10-19 16:48:58 +0000

    twentysixth commit

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -32,7 +32,7 @@ function Project(){
           }
 
           .container {
-            padding-top: 5%;
+            padding-top: 10%;
             padding-right: 15%;
             padding-left: 17%;
             background-color: #2c52b3;
