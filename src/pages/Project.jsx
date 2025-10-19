6a7a0383054be398313ff01f41d04bf36906d2b0
ryanpurnomo83 <ryanpurnomo83@gmail.com>
2025-10-19 17:05:38 +0000

    twentysixth commit

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -129,7 +129,7 @@ function Project(){
             }
 
             .project-section{
-              padding-top: 2150px !important;
+              padding-top: 1950px !important;
             }
 
             .container{
